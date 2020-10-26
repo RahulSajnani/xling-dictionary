9c44f6b4fc9cc36008ae1d7e78180b698a170efb
RahulSajnani <rahulsajnani2010@hotmail.com>
2020-10-27 04:18:12 +0530

    Update trainer

--- xling-network/create_index.py
+++ xling-network/create_index.py
@@ -15,18 +15,21 @@ import fasttext
 import argparse
 
 def create_index(fasttext_model_path, index_path, vocab_path, cache_dir, batch_size=128):
+    
+
+    
     ft_model = fasttext.load_model(fasttext_model_path)
-    words = ft_model.words
+    words = ft_model.words #["rahul", "player", "boy"]
 
     tokenizer = AutoTokenizer.from_pretrained("ai4bharat/indic-bert", max_seq_length=5)
     model = AutoModel.from_pretrained("ai4bharat/indic-bert", cache_dir=cache_dir)
 
-    index = faiss.IndexFlatL2(model.hidden_size)
+    index = faiss.IndexFlatL2(model.config.hidden_size)
     i = 0
     while i < len(words):
         batch = words[i:i + batch_size]
         tokens = tokenizer(batch)
-        outputs = model(tokens)
+        outputs = model(torch.tensor(tokens["input_ids"]))
         embeddings = torch.mean(outputs[1], 1).detach().numpy()
         index.add(embeddings)
         i += batch_size
@@ -37,11 +40,12 @@ def create_index(fasttext_model_path, index_path, vocab_path, cache_dir, batch_s
         f.write('\n'.join(words) + '\n')
     
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(add_help=True)
-    parser.add_argument("fasttext_model", type=str)
-    parser.add_argument("lang", type=str)
-    parser.add_argument("index_dir", type=str)
-    parser.add_argument("encoder_cache_dir", type=str)
+    parser.add_argument("--fasttext_model", type=str)
+    parser.add_argument("--lang", type=str)
+    parser.add_argument("--index_dir", type=str)
+    parser.add_argument("--encoder_cache_dir", type=str)
 
     args = parser.parse_args()
 
--- xling-network/trainer.py
+++ xling-network/trainer.py
@@ -13,26 +13,28 @@ class XlingualDictionary(pl.LightningModule):
     '''
 
     def __init__(self, xling_encoder, map_network, learning_rate=1e-3, dataset_path="../data/temp_data.json"):
+        
         super().__init__()
+        
         self.save_hyperparameters()    
         self.encoder = xling_encoder
         self.map = map_network
 
     def forward(self, x, target_lang, index_paths):
+        
         outputs = self.encoder(x)
         sequence_outputs = outputs[0]
         sequence_embedding = torch.mean(sequence_outputs, 1)
         y_hat = self.map(sequence_embedding)
         index = faiss.read_index(index_paths[target_lang])
         D, I = index.search(y_hat, 1)
-        return I
+        return y_hat, I
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        outputs = self.encoder(x)
-        sequence_outputs = outputs[0]
-        sequence_embedding = torch.mean(sequence_outputs, 1)
-        y_hat = self.map(sequence_embedding)
+
+
+        x, y =  batch["phrase"], batch["target"]
+        y_hat, I = self.forward(x)
         loss = F.mse_loss(y_hat, y)
 
         self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
@@ -40,11 +42,9 @@ class XlingualDictionary(pl.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        outputs = self.encoder(x)
-        sequence_outputs = outputs[0]
-        sequence_embedding = torch.mean(sequence_outputs, 1)
-        y_hat = self.map(sequence_embedding)
+        
+        x, y =  batch["phrase"], batch["target"]
+        y_hat, I = self.forward(x)
         loss = F.mse_loss(y_hat, y)
         self.log('val_loss', loss)
 
@@ -53,14 +53,14 @@ class XlingualDictionary(pl.LightningModule):
         '''
         Configure optimizers for lightning module
         '''
-        return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+        return torch.optim.Adam(self.map.parameters(), lr=self.hparams.learning_rate)
 
-    # def train_dataloader(self) -> DataLoader:
-    #     '''
-    #     Configure train dataloader
-    #     '''
-    #     dataset = data_loader.XLingualLoader(dataset_path=dataset_path)
-    #     data_loader = DataLoader(dataset, batch_size = 32, shuffle=True)
+    def train_dataloader(self) -> DataLoader:
+        '''
+        Configure train dataloader
+        '''
+        dataset = data_loader.XLingualLoader(dataset_path=dataset_path)
+        data_loader = DataLoader(dataset, batch_size = 32, shuffle=True)
 
     @staticmethod
     def add_model_specific_args(parent_parser):
