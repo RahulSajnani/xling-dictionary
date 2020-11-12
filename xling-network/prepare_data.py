bea02e408bd1e4f5b9250ef21e00b3e1151e13d1
RAHUL SAJNANI <rahul@neon.rrc.iiit.ac.in>
2020-11-13 02:33:10 +0530

    Update prepare script

--- xling-network/prepare_data.py
+++ xling-network/prepare_data.py
@@ -16,23 +16,23 @@ def create_embeddings(dictionary_path, output_directory, encoder_cache,  validat
     for lang in lang_map:
         words_dictionary[lang_map[lang]] = []
 
-    # separate language target words    
+    # separate language target words
     for l_dict in train_dictionary:
-        
+
         target_id = l_dict["Target_ID"]
         target_word = l_dict["Target_keyword"]
         words_dictionary[lang_map[target_id]].append(target_word)
-    
+
     if validation_path is not None:
-    
-        val_dictionary = read_json_file(validation_path)
-        
+
+        validation_dictionary = read_json_file(validation_path)
+
         for l_dict in validation_dictionary:
-        
+
             target_id = l_dict["Target_ID"]
             target_word = l_dict["Target_keyword"]
             words_dictionary[lang_map[target_id]].append(target_word)
-        
+
 
 
     if not os.path.exists(output_directory):
@@ -41,21 +41,27 @@ def create_embeddings(dictionary_path, output_directory, encoder_cache,  validat
     # create embeddings
     for lang in lang_map:
 
-        create_index(words_dictionary[lang_map[lang]], 
+        create_index(words_dictionary[lang_map[lang]],
                     os.path.join(output_directory, lang_map[lang] + ".index"),
                     os.path.join(output_directory, lang_map[lang] + ".vocab"),
                     encoder_cache)
 
+
+        with open(os.path.join(output_directory, lang_map[lang] + ".vocab"), 'w') as fp:
+            for listitem in words_dictionary[lang_map[lang]]:
+
+                fp.write('%s\n' % listitem)
+
 if __name__ == "__main__":
 
-    ############################# Argument Parser 
+    ############################# Argument Parser
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", help = "Training dictionary path", required = True)
     parser.add_argument("--output", help = "Output directory to save embeddings", required = True)
     parser.add_argument("--cache", help = "Bert cache", required = True)
     parser.add_argument("--val", help = "validation", required = False, default=None)
-    
+
     args = parser.parse_args()
 
     #############################################
